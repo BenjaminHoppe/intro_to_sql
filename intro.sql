c5580273fef9b1c4faaa3790381b54a338b49c07
Benjamin <hoppeconnect@gmail.com>
2018-11-28 09:06:26 -0500

    completed assignment

--- intro.sql
+++ intro.sql
@@ -15,18 +15,16 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
-
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
 
 SET search_path = public, pg_catalog;
 
@@ -346,4 +344,3 @@ ALTER TABLE ONLY robots
 --
 -- PostgreSQL database dump complete
 --
-
